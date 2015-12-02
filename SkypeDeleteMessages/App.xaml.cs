eef9ebae8f9bcdd4866f405f6256c511a36e44e6
Литвинов Андрей <Andrey.Litvinov@aplana.com>
2015-12-03 01:06:05 +0300

    intermediate commit

--- SkypeDeleteMessages/App.xaml.cs
+++ SkypeDeleteMessages/App.xaml.cs
@@ -34,5 +34,26 @@ namespace SkypeDeleteMessages
 			}
 			//conn.Dispose();
 		}
+		// Create OpenFileDialog 
+		//Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+		//// Set filter for file extension and default file extension 
+		//dlg.DefaultExt = ".png";
+		//dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif"; 
+
+
+		//// Display OpenFileDialog by calling ShowDialog method 
+		//Nullable<bool> result = dlg.ShowDialog();
+
+
+		//// Get the selected file name and display in a TextBox 
+		//if (result == true)
+		//{
+		//	// Open document 
+		//	string filename = dlg.FileName;
+		//	textBox1.Text = filename;
+		//}
 	}
 }
